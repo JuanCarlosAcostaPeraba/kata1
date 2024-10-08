86592893966609c1db150638060543ffc3ded0a0
JuanCarlosAcostaPeraba <juancarlos.ap.dev@gmail.com>
2024-10-08 01:50:25 +0100

    Add Main class

--- src/main/java/software/ulpgc/Main.java
+++ src/main/java/software/ulpgc/Main.java
@@ -1,4 +1,10 @@
 package software.ulpgc;
 
+import java.time.LocalDate;
+
 public class Main {
+    public static void main(String[] args) {
+        Person juanCarlos = new Person("Juan Carlos", LocalDate.of(1998, 12, 3));
+        System.out.println(juanCarlos);
+    }
 }
